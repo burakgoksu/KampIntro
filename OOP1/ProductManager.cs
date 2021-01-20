42d7782d3ce553a37d23bb85f2d84338b7ab9a8c
BURAK GÖKSU <gogsub58365@gmail.com>
2021-01-20 23:51:47 +0300

    20 ocak ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +" başarıyla eklendi1");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+
+       
+    }
+}
